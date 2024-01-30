4ae072cebd07d1223982e6a310002320bab59143
Ivaylo Gugalov <ivaylogugalov@gmail.com>
2024-01-30 19:44:49 +0200

    fixing yield return type issue

--- src/app/_providers/redux/products/saga.ts
+++ src/app/_providers/redux/products/saga.ts
@@ -1,4 +1,4 @@
-import { takeEvery, put, takeLatest, call, delay } from 'redux-saga/effects'
+import { put, takeLatest, call, delay } from 'redux-saga/effects'
 
 import * as Actions from './actions'
 import * as api from './api'
@@ -6,7 +6,7 @@ import { ActionTypes, SearchProductType } from './types'
 
 function* getProducts() {
   console.log('fetching products...')
-  let data = yield call(api.getProducts)
+  let data: Response = yield call(api.getProducts)
   data = yield data.json()
   console.log('action is called', data)
   yield put(Actions.listProductsSuccess(data))
@@ -14,7 +14,7 @@ function* getProducts() {
 
 function* searchProducts({ payload }: SearchProductType) {
   yield delay(500)
-  let result = yield call(api.searchProduct, payload.query)
+  let result: Response = yield call(api.searchProduct, payload.query)
   result = yield result.json()
   console.log('action is called', result)
   yield put(Actions.listProductsSuccess(result))
